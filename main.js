326255cc49f375ee93012bee2ba0e7f549acf5f2
Ramsey Nasser <ram@nas.sr>
2016-09-07 12:58:13 -0400

    Implement electron specifics, new gist export
    Moving away from streaming commits to a gist. We just export a single
    state of the board as a gist or HTML file now.

---
+++ main.js
@@ -0,0 +1,24 @@
+const {BrowserWindow, app} = require('electron');
+const os = require('os');
+const path = require('path');
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({width: 800, height: 600});
+  win.loadURL(`file:///${__dirname}/index.html`);
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
